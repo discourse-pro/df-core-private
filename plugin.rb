f93f3c78f90582f19ee049ba9e1097e27057d1f0
Dmitrii Fediuk <dfediuk@gmail.com>
2024-09-25 09:09:01 +0100

    https://github.com/discourse-pro/df-core-private/issues/21

--- plugin.rb
+++ plugin.rb
@@ -39,13 +39,6 @@ after_initialize do
 		alias_method :core__default_results, :default_results
 		def default_results(options={})
 			options[:no_definitions] = false
-			# 2024-09-25
-			# "Order topics on the home page by the number of views by default":
-			# https://github.com/discourse-pro/df-core-private/issues/21
-			filter = (options[:filter] || options[:f])
-			if filter.blank? && options[:order].nil?
-				options[:order] = 'views'
-			end
 			core__default_results options
 		end
 	end
